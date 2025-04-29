ca42c8bd74681ff82640ebdb9671f703281895bd
divyagoyal3 <goyaldivya631@gmail.com>
2025-04-29 18:48:42 +0530

    day15

---
+++ Zaptro/vite-project/src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes,Router } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Product'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [location, setLocation] = useState();
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const { latitude, longitude } = pos.coords
+      // console.log(latitude, longitude);
+
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+      try {
+        const location = await axios.get(url)
+        const exactLocation = location.data.address
+        setLocation(exactLocation)
+        setOpenDropdown(false)
+        // console.log(exactLocation);
+
+      } catch (error) {
+        console.log(error);
+
+      }
+
+    })
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+  return (
+   <>
+   <BrowserRouter>
+   <Navbar  location={location} getLocation={getLocation} openDropdown={openDropdown} setOpenDropdown={setOpenDropdown}/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/products' element={<Products/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/cart' element={<Cart/>}/>
+    <Route path='*' element={<h1>404 Not Found</h1>}/>
+   </Routes>
+   </BrowserRouter>
+   </>
+  )
+}
+
+export default App
---
+++ Zaptro/vite-project/src/context/DataContext.jsx
@@ -0,0 +1,31 @@
+import { createContext } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+
+
+export const DataContext = createContext(null);
+
+
+export const DataProvider = ({children}) => { 
+  const [data, setData] = useState();
+    // fetching all products from api
+    const fetchAllProducts = async () => {
+        try {
+           const res = await axios.get('https://fakestoreapi.in/api/products?limit=150')
+           console.log(res);
+           const productsData = res.data.products
+           setData(productsData)
+           
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+  return (
+    <DataContext.Provider value={{ data, setData, fetchAllProducts }}>
+      {children}
+    </DataContext.Provider>
+  );
+}    
+
